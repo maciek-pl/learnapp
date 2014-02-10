20ab5af238fc01c2ac72f0bb782f4571d1ea063a
Antek <Antek@Maciejs-MacBook-Pro.local>
2014-02-10 10:57:33 +0100

    Learapp application created and status methods added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Learnapp::Application.config.secret_key_base = '87e80628b6a73a5435bf46b6edc1feb3aa9335f84b3830600f20d2311fb582ccebdd3ad7129df92998127f089d517729ffae00f1c4d649abec2280c36e7a69c7'
